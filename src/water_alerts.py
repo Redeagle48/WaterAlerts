88c1e9214d7e69766ecaf52693dea897030acf2f
António Pereira <antoniopereira350@gmail.com>
2024-12-26 16:27:40 +0000

    Improved logs

--- src/water_alerts.py
+++ src/water_alerts.py
@@ -11,28 +11,37 @@ import configurations_processor as configs
 logging.basicConfig(level=logging.INFO)
 
 def log_and_exit(message, level="info", exit_code=0):
-    log_message = f"{message} Finished at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} ================="
+    log_message = f"{message}"
     if level == "error":
         logging.error(log_message)
     else:
         logging.info(log_message)
+    
+    logging.info(f"# Finished at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    logging.info("##############################################")
     sys.exit(exit_code)
 
 if __name__ == "__main__":
-    logging.info(f"Running at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')} =================")
+
+    logging.info("##############################################")
+    logging.info(f"# Running at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
     try:
+        logging.info("[Phase: scrape text]")
         text = myscrape.scrape_roturas()
         if not text:
             log_and_exit("Failed to scrape text.", level="error", exit_code=1)
      
+        logging.info("[Phase: Store text]")
         isNewValue = store_scrappingValue.processValue(text)
         if not isNewValue:
             log_and_exit("No new value found. Exiting.", exit_code=0)
         
+        logging.info("[Phase: OpenAI]")
         processedValue_json = openai_client.generate_water_outage_info(text)
 
         if processedValue_json is not None:
+            logging.info("[Phase: MQTT]")
             MQTT_TOPIC = configs.get_mqtt_configs_as_json()["topic"]
             if processedValue_json['HasWaterOutage']:
                 MQTT_client.publish_to_mqtt(MQTT_TOPIC, json.dumps(processedValue_json))
